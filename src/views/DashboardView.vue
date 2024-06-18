eebf284f74a0288880bd581a264ca6647b35995e
bowo rahayu <rahayuributwibowo@gmail.com>
2024-06-19 01:27:59 +0700

    fixed logic auth token and refresh token

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,16 +2,12 @@
 
 // logout function with remove cookies and redirect to login page. but before that, show a sweet alert
 import { logout } from '@/utils/helper.js'
-
-const handleLogout = () => {
-  logout() // logout function from helper.js
-}
 </script>
 
 <template>
   <div class="container">
     <h1>Dashboard</h1>
-    <button class="btn btn-danger" @click="handleLogout">Logout</button>
+    <button class="btn btn-danger" @click="logout">Logout</button>
   </div>
 </template>
 
